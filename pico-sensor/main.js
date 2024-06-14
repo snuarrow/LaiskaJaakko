be63b345c107e52881ced2328c081b8c1dd217c5
snuarrow <snuarrow@users.noreply.github.com>
2024-06-14 19:31:18 +0300

    cloud updates enabled

--- pico-sensor/main.js
+++ pico-sensor/main.js
@@ -31,3 +31,48 @@ function toggleLed() {
     }
     xhr.send();
 }
+
+function update_software() {
+    console.log("update software");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/update_software", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            try {
+                var response = JSON.parse(xhr.responseText);
+                console.log(response);
+            } catch (e) {
+                console.log("Error parsing JSON response", e);
+            }
+        }
+    }
+    xhr.send();
+}
+
+function check_updates_available() {
+    console.log("check updates available");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/updates_available", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            try {
+                var response = JSON.parse(xhr.responseText);
+                console.log(response);
+                updates_available = response.updates_available;
+                if (updates_available) {
+                    document.getElementById('updates_available').innerHTML = "Updates available";
+                    document.getElementById('update_button').innerHTML = "Update";
+                    document.getElementById('update_button').onclick = update_software;
+                } else {
+                    document.getElementById('updates_available').innerHTML = "No updates available";
+                    document.getElementById('update_button').innerHTML = "Check for available updates";}
+                    document.getElementById('update_button').onclick = check_updates_available;
+            } catch (e) {
+                console.log("Error parsing JSON response", e);
+            }
+        }
+    }
+    xhr.send();
+}
